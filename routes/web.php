16b019497b96c268868da354e82a497834d933f0
Cem Ünalan <unalancem@gmail.com>
2017-08-20 16:32:28 +0300

    removed an inline route

--- routes/web.php
+++ routes/web.php
@@ -7,32 +7,4 @@ use Intervention\Image\ImageManagerStatic as Image;
 Route::get('/', 'RuinsController@home');
 Route::get('/{ruin}', 'RuinsController@home');
 Route::get('/{lang}/{ruin}', 'RuinsController@home');
-
 Route::get('/sitemap', 'RuinsController@index');
-
-Route::get('/tools/get-images', function () {
-    set_time_limit(0);
-    Image::configure(array('driver' => 'imagick'));
-    $ruins = Ruin::all();
-    foreach ($ruins as $ruin) {
-        $pathToSave = base_path("public/img/ruins/{$ruin->slug}.jpg");
-
-        if (file_exists($pathToSave)) {
-            continue;
-        }
-
-        try {
-            $ruinImage = Image::make($ruin->image);
-        } catch (NotReadableException $e) {
-            echo $ruin->slug . " " .  $e->getMessage() . '<br>';
-            continue;
-        }
-
-        $ruinImage->fit(800, null, function ($constraint) {
-            $constraint->upsize();
-        });
-        $ruinImage->save($pathToSave);
-        $ruin->image = $ruin->slug . ".jpg";
-        $ruin->save();
-    }
-});
