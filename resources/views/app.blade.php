5abe014a925909ba5fb08aecb9faf3f09f6e8c67
Cem Ünalan <unalancem@gmail.com>
2017-08-28 20:57:42 +0300

    Change the meta description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('title')</title>
     <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/>
     <meta name="keywords" content="ancient cities, antique cities, turkey, ruins, historical sites">
-    <meta name="description" content="This is a map that helps you locate ancient cities in Turkey and provides useful links for the location.">
+    <meta name="description" content="Map of Ancient Cities in Turkey. You can get discover new places with useful links to history, visiting and much more.">
     <link href='{{ asset('css/mapbox-gl.css') }}' rel='stylesheet'/>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/vendor/require.js') }}" data-main="{{ asset('js/app') }}"></script>
