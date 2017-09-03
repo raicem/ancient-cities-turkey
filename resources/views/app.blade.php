2981df5c3d19d2d20e7c9be7a6de6253d99bcf50
Cem Ünalan <unalancem@gmail.com>
2017-09-03 08:36:24 +0100

    added twitter card

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,11 @@
     <title>@yield('title')</title>
     <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/>
     <meta name="keywords" content="ancient cities, antique cities, turkey, ruins, historical sites">
-    <meta name="description" content="Map of Ancient Cities in Turkey. You can get discover new places with useful links to history, visiting and much more.">
+    <meta name="description" content="Map of Ancient Cities in Turkey. You can get discover new places with useful links to history, visiting info and much more.">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Ancient Cities of Turkey">
+    <meta name="twitter:description" content="Map of Ancient Cities in Turkey. You can get discover new places with useful links to history, visiting info and much more.">
+    <meta name="twitter:image" content="/img/preview.jpeg">
     <link href='{{ asset('css/mapbox-gl.css') }}' rel='stylesheet'/>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('head-scripts')
