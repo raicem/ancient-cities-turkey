45cf883f6e5f9e8fa8bfd58554c4127115eed452
Cem Ünalan <unalancem@gmail.com>
2017-09-03 19:43:11 +0100

    add contact info

--- resources/views/partials/about-handlebars.blade.php
+++ resources/views/partials/about-handlebars.blade.php
@@ -6,6 +6,7 @@
     <p>I wanted to be able see all of the ancient cities in Turkey on a map so that I can discover and travel whichever is close to my location. I could not find anything similar to what I've wanted so I started building this web site. Currently this contains around 80 archeological sites in Turkey. I assume the maximum number would be around 140 so there is still a lot of points to add.</p>
     <p>If you'd like to read more about this project you can read my blog post <a data-bypass href="https://raicem.github.io/2017/08/28/ancient-cities-of-turkey/">here.</a></p>
     <p>I have tried to pick most accurate information about the ruins. But as I have no formal education on this subject, please consider that there may be some data that is not up-to-date or correct. Please feel free to inform me using the send feedback form.</p>
+    <p>You can reach me via the report issue form, <a data-bypass href="https://twitter.com/ciftehavuz">Twitter</a> or <a data-bypass href="mailto:unalancem@gmail.com">unalancem@gmail.com</a>.</p>
     <h3>About me</h3>
     <p>
         My name is Cem Ünalan. I'm a back-end developer working at <a href="http://atolye15.com">Atölye15.</a>
@@ -18,7 +19,8 @@
     <h3>Bu proje hakkında</h3>
     <p>Türkiye'nin tüm antik kentlerini bir harita üzerinde görmek istedim. Böylece yakınlarda ne var ne yok görebilecek ve ona göre seyahat edebilecektim. İstediğime yakın bir şey bulamadığımdan dolayı, bu siteyi yapmaya başladım. Şu anda sitede 80 adet ören yeri bulunmakta. Ancak tahminlerime göre bu sayı 140'a kadar çıkabilir. Yani hala ekleyecek bolca nokta var.</p>
     <p>Proje hakkında daha fazla bilgi almak isterseniz <a data-bypass href="https://raicem.github.io/2017/08/28/turkiyenin-antik-kentleri/">blog yazımı</a> ziyaret edebilirsiniz.</p>
-    <p>Topladığım verilerin doğruluğuna özen gösterdim ancak bu konular hakkında herhangi bir resmi eğitimim olmadığı için, lütfen bazı bilgilerin güncel veya doğru olmayabileceğini göz önünde bulundurun. Bu durumlarda hata bildir formunu kullanarak bana ulaşabilirsiniz.</p>
+    <p>Topladığım verilerin doğruluğuna özen gösterdim ancak bu konular hakkında herhangi bir resmi eğitimim olmadığı için, lütfen bazı bilgilerin güncel veya doğru olmayabileceğini göz önünde bulundurun.</p>
+    <p>Sitedeki hata bildir formundan, <a data-bypass href="https://twitter.com/ciftehavuz">Twitter'dan</a> veya <a data-bypass href="mailto:unalancem@gmail.com">unalancem@gmail.com</a> adresinden bana ulaşabilirsiniz.</p>
     <h3>Hakkımda</h3>
     <p>
         Benim adım Cem Ünalan. <a data-bypass href="http://atolye15.com">Atölye15'te</a> back-end geliştirici olarak çalışmaktayım. 
