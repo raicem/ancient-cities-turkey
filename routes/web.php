04fd7cd1ee993611205580c65ba20eb290da2800
Cem Ünalan <unalancem@gmail.com>
2017-09-10 09:23:30 +0300

    delete a testing route

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use App\Ruin;
 
-Route::get('/testing', function () {
-    $ruins = Ruin::all();
-    $ruins->map(function (Ruin $item) {
-        $item->image = 'img/ruins/' . $item->image;
-        $item->save();
-    });
-});
 Route::get('/sitemap', 'RuinsController@sitemap');
 Route::get('/', 'RuinsController@index');
 Route::get('/{ruin}', 'RuinsController@index');
