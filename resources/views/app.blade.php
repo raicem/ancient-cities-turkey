dafbb4786d0a79057c4a5d266ca8722d64a80f65
Cem Ünalan <unalancem@gmail.com>
2017-12-17 22:17:52 +0300

    Removed the script duplication

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,8 +23,8 @@
 
 <body>
 
-	@yield('content') @yield('scripts')
-	<script src="{{ mix('js/app.js') }}"></script>
+	@yield('content') 
+	@yield('scripts')
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
