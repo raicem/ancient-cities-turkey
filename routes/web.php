309717298ef30ffa9d7e5be89ff6bdc62a0a0cf2
Cem Ünalan <unalancem@gmail.com>
2017-12-17 22:12:58 +0300

    Route fix for the ruins/show page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Ruin;
 
 Route::get('/sitemap', 'RuinsController@sitemap');
 Route::get('/{locale?}', 'RuinsController@index');
-Route::get('/{locale}/{ruin}', 'RuinsController@show');
+Route::get('/{locale}/{ruin}', 'RuinsController@index');
