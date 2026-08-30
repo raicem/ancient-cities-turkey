063b074128944623751de8f797cce4e8da47c99e
raicem <unalancem@gmail.com>
2026-08-30 14:32:46 +0300

    Run links:check monthly instead of weekly

--- routes/console.php
+++ routes/console.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Schedule;
 */
 
 Schedule::command('sitemap:generate')->weeklyOn(5, '19:00');
-Schedule::command('links:check')->weeklyOn(5, '20:00');
+Schedule::command('links:check')->monthlyOn(5, '20:00');
